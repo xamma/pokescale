edc9e543fda803f6ae18517f34f78ec8f4541289
Xamma <Xam93@live.de>
2023-08-24 10:57:36 +0200

    Fixed test case...

--- src/main/api_test.py
+++ src/main/api_test.py
@@ -11,7 +11,7 @@ def test_get_test():
 def test_render_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert "Pokémon scale" in response.text
+    assert "Pokéscale" in response.text
 
 def test_render_pokemon_valid():
     response = client.post("/pokemon", data={"pokemon_name": "pikachu"})
